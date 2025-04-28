3d448ca778882287b83f425cfd670a1e5725356c
SwathiPriya37 <rswathipriya3@gmail.com>
2025-04-28 22:56:50 +0530

    added const

---
+++ script.js
@@ -0,0 +1,8 @@
+const passwordBox= document.getElementById("Password")
+const length = 12;
+
+const upperCase= "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+const lowerCase= "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "@!#$%^&*(){}[]_-=+></?~"
+
